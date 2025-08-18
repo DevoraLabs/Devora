d122451d8f0c69a7f9b1ccd3ed3dff3883ac268b
mykhailoko <ivashevbrd@gmail.com>
2025-08-18 20:23:06 +0300

    add left navbar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import Navbar from "../Navbar/Navbar"
 import "./Header.css"
 import CreateStartupModal from "../CreateStartupModal/CreateStartupModal"
+import LeftNavbar from "../LeftNavbar/LeftNavbar"
 
 function Header() {
     const [createStartup, setCreateStartup] = useState(false)
@@ -9,6 +10,9 @@ function Header() {
     return (
         <div className='header'>
             <Navbar />
+            <LeftNavbar />
+            <div className="header-top-divider"></div>
+            <div className="header-left-divider"></div>
 
             <button 
                 className="create-startup-button" 
---
+++ frontend/src/components/LeftNavbar/LeftNavbar.jsx
@@ -0,0 +1,43 @@
+import "./LeftNavbar.css"
+import Home from '../../assets/home.png'
+import Startups from '../../assets/startups.png'
+import MyStartups from '../../assets/my-startups.png'
+import Profile from '../../assets/profile.png'
+import { Link } from "react-router-dom"
+import { useUser } from "../../context/UserContext"
+
+
+function LeftNavbar() {
+    const { username } = useUser()
+
+    return (
+        <ul className='left-navbar'>
+            <li className='left-navbar-item'>
+                <Link to="/">
+                    <img src={Home} alt='home' />
+                    <h2>Главная</h2>
+                </Link>
+            </li>
+            <li className='left-navbar-item'>
+                <Link>
+                    <img src={Startups} alt='startups' />
+                    <h2>Стартапы</h2>
+                </Link>
+            </li>
+            <li className='left-navbar-item'>
+                <Link>
+                    <img src={MyStartups} alt='my-startups' style={{width: "25px"}} />
+                    <h2>Мои стартапы</h2>
+                </Link>
+            </li>
+            <li className='left-navbar-item'>
+                <Link to={`/user/${username}`}>
+                    <img src={Profile} alt='profile' style={{height: "23px"}} />
+                    <h2>Мой профиль</h2>
+                </Link>
+            </li>
+        </ul>
+    )
+}
+
+export default LeftNavbar
